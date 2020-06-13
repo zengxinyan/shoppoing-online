8ffb442491ae56e35ed641f3827006b73c9e2551
ZengXinYan <1434008709@qq.com>
2020-06-13 22:27:26 +0800

    按商品名、价格排序，按商品名、商品描述进行搜索

--- src/main/java/com/demo/springboot/helloworld/controller/GoodsController.java
+++ src/main/java/com/demo/springboot/helloworld/controller/GoodsController.java
@@ -1,42 +1,53 @@
 package com.demo.springboot.helloworld.controller;
 
 import com.demo.springboot.helloworld.common.domain.Category;
+import com.demo.springboot.helloworld.common.domain.Goods;
 import com.demo.springboot.helloworld.common.utils.Result;
 import com.demo.springboot.helloworld.service.GoodsService;
+import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
 @Controller
 public class GoodsController {
     @Autowired
     private GoodsService goodsService;
 
+    @RequestMapping("/getSearchGoods")
+    @ResponseBody
+    public Object getSearchGoods(@RequestParam String searchString,@RequestParam String sortName,@RequestParam String sortOrder,@RequestParam(defaultValue = "1") int pageNo, @RequestParam(defaultValue = "10")int pageSize){
+        PageInfo<Goods> goods = goodsService.findSearchGoods(searchString,sortName,sortOrder,pageNo,pageSize);
+        return Result.success(goods ,"分页 查询book 对象");
+    }
+
     @RequestMapping("/getAllGoods")
     @ResponseBody
-    public Object getAllGoods(@RequestParam(defaultValue = "1") int pageNo, @RequestParam(defaultValue = "10")int pageSize){
-        System.out.println("1111111");
-        return Result.success(goodsService.findAllGoods(pageNo,pageSize),"分页 查询book 对象");
+    public Object getAllGoods(@RequestParam String sortName,@RequestParam String sortOrder,@RequestParam(defaultValue = "1") int pageNo, @RequestParam(defaultValue = "10")int pageSize){
+        PageInfo<Goods> goods = goodsService.findAllGoods(sortName,sortOrder,pageNo,pageSize);
+        return Result.success(goods ,"分页 查询book 对象");
     }
 
     @RequestMapping("/getHotGoods")
     @ResponseBody
-    public Object getHotGoods(@RequestParam(defaultValue = "1") int pageNo, @RequestParam(defaultValue = "10")int pageSize){
+    public Object getHotGoods(@RequestParam String sortName,@RequestParam String sortOrder,@RequestParam(defaultValue = "1") int pageNo, @RequestParam(defaultValue = "10")int pageSize){
         System.out.println("hot");
-        return Result.success(goodsService.findHotGoods(pageNo,pageSize),"分页 查询book 对象");
+        return Result.success(goodsService.findHotGoods(sortName,sortOrder,pageNo,pageSize),"分页 查询book 对象");
     }
 
     @RequestMapping("/getDiscountGoods")
     @ResponseBody
-    public Object getDiscountGoods(@RequestParam(defaultValue = "1") int pageNo, @RequestParam(defaultValue = "10")int pageSize){
-        return Result.success(goodsService.findDiscountGoods(pageNo,pageSize),"分页 查询book 对象");
+    public Object getDiscountGoods(@RequestParam String sortName,@RequestParam String sortOrder,@RequestParam(defaultValue = "1") int pageNo, @RequestParam(defaultValue = "10")int pageSize){
+        return Result.success(goodsService.findDiscountGoods(sortName,sortOrder,pageNo,pageSize),"分页 查询book 对象");
     }
 
     @RequestMapping("/getCategoryGoods")
     @ResponseBody
-    public Object getCategoryGoods(@RequestParam int id,@RequestParam(defaultValue = "1") int pageNo, @RequestParam(defaultValue = "10")int pageSize){
-        return Result.success(goodsService.findCategoryGoods(id,pageNo,pageSize),"分页 查询book 对象");
+    public Object getCategoryGoods(@RequestParam String sortName,@RequestParam String sortOrder,@RequestParam int id,@RequestParam(defaultValue = "1") int pageNo, @RequestParam(defaultValue = "10")int pageSize){
+        return Result.success(goodsService.findCategoryGoods(sortName,sortOrder,id,pageNo,pageSize),"分页 查询book 对象");
     }
 }
--- src/main/java/com/demo/springboot/helloworld/service/GoodsService.java
+++ src/main/java/com/demo/springboot/helloworld/service/GoodsService.java
@@ -16,32 +16,57 @@ public class GoodsService {
 
     @Autowired
     GoodsMapper goodsMapper;
-    public PageInfo<Goods> findAllGoods(int pageNo, int pageSize){
+
+    public PageInfo<Goods> findAllGoods(String sortName,String sortOrder,int pageNo, int pageSize){
         PageHelper.startPage(pageNo,pageSize);
-        List<Goods> goods = goodsMapper.selectByExample(null);
+        GoodsExample goodsExample = new GoodsExample();
+        goodsExample.setOrderByClause(sortName+' '+sortOrder);
+        List<Goods> goods = goodsMapper.selectByExample(goodsExample);
         return new PageInfo<>(goods);
     }
 
-    public PageInfo<Goods> findHotGoods(int pageNo, int pageSize) {
+    public PageInfo<Goods> findHotGoods(String sortName,String sortOrder,int pageNo, int pageSize) {
         PageHelper.startPage(pageNo,pageSize);
         GoodsExample goodsExample = new GoodsExample();
+        goodsExample.setOrderByClause(sortName+' '+sortOrder);
         goodsExample.createCriteria().andIshotEqualTo(true);
         List<Goods> goods = goodsMapper.selectByExample(goodsExample);
         return new PageInfo<>(goods);
     }
 
-    public PageInfo<Goods> findDiscountGoods(int pageNo, int pageSize) {
+    public PageInfo<Goods> findDiscountGoods(String sortName,String sortOrder,int pageNo, int pageSize) {
         PageHelper.startPage(pageNo,pageSize);
         GoodsExample goodsExample = new GoodsExample();
+        goodsExample.setOrderByClause(sortName+' '+sortOrder);
         goodsExample.createCriteria().andIsdiscountEqualTo(true);
         List<Goods> goods = goodsMapper.selectByExample(goodsExample);
         return new PageInfo<>(goods);
     }
 
-    public PageInfo<Goods> findCategoryGoods(int id,int pageNo, int pageSize) {
+    public PageInfo<Goods> findCategoryGoods(String sortName,String sortOrder,int id,int pageNo, int pageSize) {
         PageHelper.startPage(pageNo,pageSize);
         GoodsExample goodsExample = new GoodsExample();
+        goodsExample.setOrderByClause(sortName+' '+sortOrder);
         goodsExample.createCriteria().andCategoryIdEqualTo(id);
+        List<Goods> goods = goodsMapper.selectByExample(goodsExample);
+        return new PageInfo<>(goods);
+    }
+
+    public PageInfo<Goods> findSearchGoods(String searchString,String sortName,String sortOrder, int pageNo, int pageSize) {
+        PageHelper.startPage(pageNo,pageSize);
+        GoodsExample goodsExample = new GoodsExample();
+        goodsExample.setOrderByClause(sortName+' '+sortOrder);
+        GoodsExample.Criteria criteria = goodsExample.createCriteria();
+        if(searchString != null && searchString.length() > 0){
+            String s ="%";
+            for(int i=0;i<searchString.length();i++) {
+                s += "%" + searchString.charAt(i) + "%";
+            }
+            criteria.andNameLike(s);
+            GoodsExample.Criteria criteria1 = goodsExample.createCriteria();
+            criteria1.andDescriptionLike(s);
+            goodsExample.or(criteria1);
+        }
         List<Goods> goods = goodsMapper.selectByExample(goodsExample);
         return new PageInfo<>(goods);
     }
